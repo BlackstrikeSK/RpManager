48a20978c7cccebe6b969d5ea3638333cdfe7edb
BlackstrikeSK <60195039+BlackstrikeSK@users.noreply.github.com>
2021-09-09 13:17:26 +0200

    Add files via upload

---
+++ RpManager/RpManager/MtfSpawnHint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using UnityEngine;
+using MEC;
+
+namespace RpManager
+{
+    public class MtfSpawnHint
+    {
+        public void OnNtfSpawning(SpawningEventArgs ev)
+        {
+            if (ev.Player.Team == Team.MTF)
+            {
+                if (ev.Player.Role == RoleType.NtfCaptain)
+                {
+                    ev.Player.ShowHint(Plugin.Singleton.Config.NtfCommanderHint, 10);
+                }
+
+                if (ev.Player.Role == RoleType.NtfPrivate)
+                {
+                    ev.Player.ShowHint(Plugin.Singleton.Config.OtherNtfHint, 10);
+                }
+            }
+        }
+
+        
+    }
+}
